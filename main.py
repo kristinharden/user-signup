686ef8d54317cc6b615dfbf32e9d26211b1be2fd
Kristin Harden <33766992+kristinharden@users.noreply.github.com>
2018-01-09 19:35:48 -0500

    Update main.py
    Revised to correct "def sign_up()". Totally functional.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 import os
-# import jinja2
-
-# template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-# jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader
-# (template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,26 +17,6 @@ def display_form():
        verify_password = '',
        email = '' )
 
-
-# @app.route("/", methods=["POST"])
-# def sign_up():
-#     print("Try two")
-#     user = str(request.form["username"])
-#     pw = str(request.form["password"])
-#     validpw = str(request.form["verify_password"])
-#     em = str(request.form["email"])
-#     print(user)
-#     return render_template("form.html",
-#        username_error = '',
-#        password_error = '',
-#        verify_password_error = '',
-#        email_error = '',
-#        username = '',
-#        password = '',
-#        verify_password = '',
-#        email = '' )
-
-
 @app.route("/", methods=["POST"])
 def sign_up():
     user = str(request.form["username"])
@@ -54,47 +29,23 @@ def sign_up():
     vpw_error = ""
     em_error = ""
 
-    if len(user) <3 or len(user) > 20:
-        user_error = "Username must be between 3 and 20 characters long."
-    elif ' ' in user:
-        user_error = "Username may not contain spaces."
-    else:
-        user = user
+    if len(user) <3 or len(user) > 20 or " " in user:
+        user_error = "Username must be between 3 and 20 characters long and may not contain spaces."
+        user = ""
 
-    if "@" or "." not in em and len(em) > 1 or " " in em or len(em) <3 or len(em) >20:
-        em_error = "Not a valid email address."
-        em = ""
-    else:
-        em = em
+    if len(email)>0:
+        if "@" not in em or "." not in em or " " in em or len(em) <3 or len(em) >20:
+            em_error = "Not a valid email address."
+            em = ""
 
-    if pw == "":
-        pw_error = "Password required."
-        pw = ""
-        validpw = ""
-    elif len(pw) <3 or len(pw) > 20:
-        pw_error = "Password must be between 3 and 20 characters long."
-        pw = ""
-        validpw = ""
-    elif ' ' in pw:
-        pw_error = "Password may not contain spaces."
-        pw = ""
-        validpw = ""
-    elif validpw == "":
-        vpw_error = "Please validate password."
-        pw = ""
-        validpw = ""
-    elif pw != validpw:
+    if len(pw) <3 or len(pw) > 20 or " " in pw:
+        pw_error = "Password must be between 3 and 20 characters long and may not contain spaces."
+        
+    if pw != validpw:
         vpw_error = "Password entries do not match."
-        pw = ""
-        validpw = ""
-    else:
-        pw = pw
-        validpw = validpw
 
     if not user_error and not pw_error and not vpw_error and not em_error:
-        # user = user
-        return "Success"
-        # return redirect("/welcome?user={0}".format(user))
+        return render_template("welcome.html", user=user)
     else:
         return render_template("form.html",
             user_error = user_error,
@@ -105,10 +56,5 @@ def sign_up():
             pw = "",
             validpw = "",
             em = em,)
-
-# @app.route("/welcome", methods = ["GET"])
-# def welcome():
-#     user = request.args.get('user')
-#     return render_template("welcome.html")
 
 app.run()
